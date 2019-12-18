54caed99292753534f37c0d59e748ec8a16d6130
t105590017 <t105590017@ntut.org.tw>
2019-12-18 15:31:49 +0800

    bootstrap View

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use('/users', usersRouter);
 app.use('/test', testRouter);
 
 app.use('/Home', homeRouter);
+app.use('/Link', homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/Home.js
+++ routes/Home.js
@@ -3,8 +3,27 @@ var router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('mainPage', {
-        viewContent: {
-            name: 'Home/home'
+        navActive: 'Home',
+        ContentViewName:'Home/home',
+        ContentViewData: {
+            Carousel:[
+                {
+                    ImgPath:'https://goo.gl/PPDTMH',
+                    Description:'430i M Sport'
+                },
+                {
+                    ImgPath:'https://goo.gl/ZPCXA8',
+                    Description:'Ranger 2.0L Bi-Turbo Wildtrak'
+                },
+                {
+                    ImgPath:'https://goo.gl/Z15J4c',
+                    Description:'i8 Roadster'
+                },
+                {
+                    ImgPath:'https://goo.gl/Nwmeca',
+                    Description:'RX 350'
+                },
+            ]
         }
     });
 });
